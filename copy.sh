d9dcc66a5f3535326500380d6c49ba4c3ba003f7
claudietto <claudietto@users.noreply.github.com>
2026-06-16 18:09:10 +0200

    copy.sh: create narinfos directly

--- copy.sh
+++ copy.sh
@@ -5,14 +5,13 @@
 # steps:
 # 1. Generate the transitive closure of all the derivations from the specified
 #    package(s) and compute the store path for each
-# 2. From the remote, copy all the `*.narinfo` files into a scratch directory
-# 3. For each store path, try and fetch the `.narinfo` from
-#    `https://cache.nixos.org` into the scratch directory as well
-# 4. Run the actual `nix copy` command, since the target directory already
-#    contains a bunch of `.narinfo`s, all of these will be skipped and their
-#    derivation's `.nar` archive will not be generated
-# 5. Delete all the `.narinfo`s generated from step (2) and (3)
-# 6. rsync the scratch directory to the remote
+# 2. For each store path, check if it has been signed by `cache.nixos.org`, if
+#    so, skip the store path
+# 3. Copy all the `.narinfo`s from the rsync remote to a scratch directory
+# 4. Run the remaning store paths with `nix-copy.py` which will create the
+#    narinfos and nars just like `nix copy` in a scratch directory, skipping any
+#    archive that's already present
+# 5. rsync the scratch directory to the remote
 #
 # Usage:
 #   copy.sh <rsync-target> <package> [<package>...]
@@ -29,63 +28,35 @@ set -euo pipefail
 
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
-function nix_cmd() {
-    nix --extra-experimental-features "nix-command flakes" "$@"
-}
-
 # Argument parsing
 REMOTE="${1%/}"
 shift
 PACKAGES=("$@")
 
-# Temporary files
 SCRATCH_DIR="$(mktemp --tmpdir -d tmp.nix-cache-push.XXXXXXXXXX)"
-CLOSURE_FILE="$(mktemp --tmpdir tmp.nix-cache-push.closure.XXXXXXXXXX)"
-MISSING_CLOSURES="$(mktemp --tmpdir tmp.nix-cache-push.tocopy.XXXXXXXXXX)"
-cleanup() {
-  rm -rf "$SCRATCH_DIR" "$CLOSURE_FILE" "$MISSING_CLOSURES"
-}
-trap cleanup EXIT
+trap 'rm -rf "$SCRATCH_DIR"' EXIT
 
 # Given a list of desired packages, resolve them to the one or more store paths
 # in the local system
-readarray -t output_paths < <(
+readarray -t OUTPUT_PATHS < <(
   for PACKAGE in "${PACKAGES[@]}"; do
-    nix_cmd build --no-link --print-out-paths "$PACKAGE"
+    nix build --no-link --print-out-paths "$PACKAGE"
   done
 )
-nix_cmd path-info --recursive "${output_paths[@]}" > "$CLOSURE_FILE"
+readarray -t STORE_PATHS < <(
+  nix path-info --recursive --json --json-format 1 "${OUTPUT_PATHS[@]}" | \
+  jq -r 'to_entries[] | .key + " " + (.value.signatures | join(" "))' | \
+  grep -v ' cache\.nixos\.org-' | \
+  cut -d' ' -f1
+)
 
 # Pull narinfos from the remote, so paths it already has are skipped below.
 rsync \
   -a --include='/nix-cache-info' --include='/*.narinfo' --exclude='*' \
   "$REMOTE/" "$SCRATCH_DIR/"
 
-# Run the `fetch-narinfos.py` script, which will fetch all the `.narinfos`
-# it can from cache.nixos.org. The ones that cannot be fetched are printed to
-# stdout.
-nix-shell -p "python3.withPackages(ps: [ ps.niquests ])" \
-    --run "'$SCRIPT_DIR/fetch-narinfos.py' '$SCRATCH_DIR' '$CLOSURE_FILE'" \
-    > "$MISSING_CLOSURES"
-readarray -t store_paths < "$MISSING_CLOSURES"
-
-# Save which narinfos are present, these will be deleted before the scratch
-# directory is re-pushed back to the remote
-existing_narinfos=("$SCRATCH_DIR"/*.narinfo)
-
-# Actually perform the copy of the store paths. All the packages for which the
-# `.narinfo` is already present will be skipped.
-if [[ ${#store_paths[@]} -gt 0 ]]; then
-    nix_cmd copy \
-      --to "file://$SCRATCH_DIR?secret-key=$(realpath "$SECRET_KEY_FILE")" \
-      --no-recursive \
-      "${store_paths[@]}"
-fi
-
-# Delete the narinfos which were present before `nix copy` was run
-if [[ ${#existing_narinfos[@]} -gt 0 ]]; then
-    rm "${existing_narinfos[@]}"
-fi
+# Copy only the needed store paths into the scratch dir
+"$SCRIPT_DIR/nix-copy.py" "$SCRATCH_DIR" "${STORE_PATHS[@]}"
 
 # Push back the newly created files, in 2 phases:
 # 1. Everything but narinfos
---
+++ nix-copy.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+from base64 import b64decode
+from hashlib import file_digest
+from pathlib import Path
+from subprocess import PIPE, Popen, run
+from tempfile import TemporaryFile, mkstemp
+
+NIX32_ALPHABET = "0123456789abcdfghijklmnpqrsvwxyz"
+
+
+def nix32_encode(data: bytes) -> str:
+    """Encode a bytes object into a Nix32 (Nix's Base32 variant) string."""
+    length = (len(data) * 8 - 1) // 5 + 1
+    result = ""
+    for n in range(length - 1, -1, -1):
+        i, j = divmod(n * 5, 8)
+        c = data[i] >> j
+        if i < len(data) - 1:
+            c |= data[i + 1] << (8 - j)
+        result += NIX32_ALPHABET[c & 0x1F]
+
+    return result
+
+
+def convert_hash(input_: str):
+    """Convert SRI 'sha256-XXXX' hash into nix32"""
+    assert input_.startswith("sha256-")
+    hash_string = input_.removeprefix("sha256-")
+    return nix32_encode(b64decode(hash_string))
+
+
+def main():
+    output_dir = Path(sys.argv[1])
+    store_paths = sys.argv[2:]
+
+    nar_dir = output_dir / "nar"
+    nar_dir.mkdir(exist_ok=True)
+
+    run(
+        ["nix", "store", "sign", "--key-file", os.environ["SECRET_KEY_FILE"], *store_paths],
+        check=True,
+    )
+
+    with TemporaryFile() as f:
+        run(
+            ["nix", "path-info", "--json", "--json-format", "2", *store_paths], stdout=f, check=True
+        )
+        f.seek(0)
+        store_info = json.load(f)
+
+    for store_path in store_paths:
+        name = os.path.basename(store_path)
+        derivation_info = store_info["info"][name]
+        hash_ = convert_hash(derivation_info["narHash"])
+
+        if (output_dir / f"{hash_}.narinfo").exists():
+            continue
+
+        nar_fd, temp_nar_name = mkstemp(dir=nar_dir)
+        with open(nar_fd, "wb+") as f:
+            zstd_process = Popen(["zstd", "-zc", "-T0"], stdin=PIPE, stdout=f)
+            run(["nix", "nar", "pack", store_path], stdout=zstd_process.stdin, check=True)
+            zstd_process.stdin.close()
+            assert zstd_process.wait() == 0
+
+            f.seek(0)
+            compressed_hash = nix32_encode(file_digest(f, "sha256").digest())
+            compressed_size = f.seek(0, os.SEEK_END)
+
+        Path(temp_nar_name).rename(nar_dir / f"{compressed_hash}.nar.zst")
+
+        (output_dir / f"{name[:32]}.narinfo").write_text(
+            f"""
+StorePath: {store_path}
+URL: nar/{compressed_hash}.nar.zst
+Compression: zstd
+FileHash: sha256:{compressed_hash}
+FileSize: {compressed_size}
+NarHash: sha256:{hash_}
+NarSize: {derivation_info["narSize"]}
+References: {" ".join(derivation_info["references"])}
+Deriver: {derivation_info["deriver"]}
+Sig: {" ".join(derivation_info["signatures"])}
+""".strip() + "\n"
+        )
+
+    (output_dir / "nix-cache-info").write_text(f"StoreDir: {store_info["storeDir"]}")
+
+
+if __name__ == "__main__":
+    main()
